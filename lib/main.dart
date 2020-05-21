e9a28cc5790cd7a20baa3234f5648292b704fb7d
Mike Hatfield <oakraven13@gmail.com>
2020-05-21 19:28:42 -0300

    Completed lesson 190

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './providers/products_provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 
@@ -8,17 +10,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        routes: {
+          '/': (_) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
-      routes: {
-        '/': (_) => ProductsOverviewScreen(),
-        ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
-      },
     );
   }
 }
