9d55b87a6a7831ca50a5a652abcbd84cc1c7470a
Mike Hatfield <oakraven13@gmail.com>
2020-05-23 20:12:28 -0300

    Completed lesson 209

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import '../providers/orders_provider.dart' as op;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final op.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +23,53 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$ ${order.amount}'),
+            title: Text('\$ ${widget.order.amount.toStringAsFixed(2)}'),
             subtitle: Text(
-              DateFormat('MMMM d, yyyy - hh:mm a').format(order.dateTime),
+              DateFormat('MMMM d, yyyy - hh:mm a')
+                  .format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_isExpanded)
+            Container(
+              height: min(widget.order.products.length * 30.0 + 20, 100),
+              child: ListView.builder(
+                  itemCount: widget.order.products.length,
+                  itemBuilder: (_, i) {
+                    var prod = widget.order.products[i];
+
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 4,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(prod.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          Text(
+                            '${prod.quantity} x \$ ${prod.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+            )
         ],
       ),
     );
